6bdf190f534cf28e4389387193df918f35f05b56
Iñaki Respaldiza <irespaldiza@okteto.com>
2020-12-28 19:12:55 +0100

    test

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.50.0"
+    }
+  }
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "okteto-test"
@@ -7,4 +13,11 @@ terraform {
       prefix = "test-"
     }
   }
+}
+
+provider "google" {
+  credentials = var.gcp_credentials
+  # access_token = var.gcp_access_token
+  project = var.gcp_project
+  region  = var.gcp_region
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
-variable "workspace" {
-  default = "test"
+variable "gcp_credentials" {
+  type        = string
+  description = "(optional) describe your variable"
 }
 
-locals {
-  workspace = var.workspace
+variable "gcp_project" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "gcp_region" {
 }
